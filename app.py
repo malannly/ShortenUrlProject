a60ac0e534c3f89592d701bc5e1072042df1a22a
malannly <mamedovaanna.i@gmail.com>
2022-08-19 12:16:09 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import string, random
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Urls(db.Model):
+    sp_id = db.Column('sp_id', db.Integer, primary_key = True)
+    long = db.Column('long', db.String())
+    short = db.Column('short', db.String(8))
+    def __init__(self, long, short):
+        self.long = long
+        self.short = short
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+def shorten_url():
+    while True:
+        j = []
+        for i in range(4):
+            j.append(random.choice(string.ascii_uppercase + string.ascii_lowercase))
+            j.append(str(random.randint(0,9)))
+        previos = ''.join(j)
+        short_url = Urls.query.filter_by(short = previos).first()
+        if not short_url:
+            return previos
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    if request.method == 'POST':
+        url_recieved = request.form['nm']
+        found_url = Urls.query.filter_by(long = url_recieved).first()
+        if found_url:
+            return redirect(url_for('display_short_url',url = found_url.short))
+        else:
+            short_url = shorten_url()
+            new_url = Urls(url_recieved, short_url)
+            db.session.add(new_url)
+            db.session.commit()
+            return redirect(url_for('display_short_url',url = short_url))
+    else:
+        return render_template('home.html')
+
+@app.route('/display/<url>')
+def display_short_url(url):
+    return render_template('shorturl.html', short_url_display = url)
+
+@app.route('/<short_url>')
+def redirection(short_url):
+    long_url = Urls.query.filter_by(short=short_url).first()
+    if long_url:
+        return redirect(long_url.long)
+    else:
+        return f'<h2>Ops! This url doesn`t exist</h2>'
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
