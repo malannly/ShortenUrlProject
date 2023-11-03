20211e2f9c34eee777a9a4dbab2e94c7e930c95d
malannly <mamedovaanna.i@gmail.com>
2023-11-03 21:32:23 +0200

    .gitignore has to work

---
+++ tasks/task.py
@@ -0,0 +1,9 @@
+from tasks.celery_app import app
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+
+@app.task(name='delete_url')
+def delete_url(url):
+    db.session.delete(url)
+    db.session.commit()
