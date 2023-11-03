7a06649abab8ff7df87b2a351c5e13517cc02dbd
malannly <mamedovaanna.i@gmail.com>
2023-11-03 21:14:27 +0200

    ignorefile

--- celery_app.py
+++ celery_app.py
@@ -3,10 +3,10 @@ from celery.schedules import crontab
 
 #broker='sqla+sqlite:///urlsdb.sqlite'
 
-app = Celery('celery_app', broker='sqla+sqlite:///celerydb.sqlite', include=['tasks'])
+app = Celery('celery_app', broker='redis://127.0.0.1:6379/0', include=['tasks'])
 app.conf.beat_schedule = {
-    'test' : {
-        'task' : 'test',
+    'delete_url' : {
+        'task' : 'delete_url',
         'schedule' : crontab(minute='*/1'),
 },
 }
